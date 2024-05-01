8204d2cf22cf3b1b49781fd3d42d5eef2bdf83c7
Ahmed Mahfouz <ahmed.ahmed240@edu.menofia.edu.eg>
2024-05-01 14:22:11 +0300

    fix the Account class

--- SOLID-Principles/Account.cs
+++ SOLID-Principles/Account.cs
@@ -1,20 +1,24 @@
 ﻿namespace SOLID_Principles
 {
-    public class Account
+    public abstract class AccountBase
     {
         public decimal Balance { get; protected set; }
-        public virtual decimal WithdrawalAmount { get; protected set; }
 
         public virtual void Deposit(decimal amount)
         {
             Balance += amount;
         }
-        public virtual void Withdraw(decimal amount)
+
+        public abstract void Withdraw(decimal amount);
+    }
+
+    public class Account : AccountBase
+    {
+        public override void Withdraw(decimal amount)
         {
-            WithdrawalAmount = amount;
-            if (Balance >= WithdrawalAmount)
+            if (Balance >= amount)
             {
-                Balance -= WithdrawalAmount;
+                Balance -= amount;
             }
             else
             {
@@ -23,18 +27,28 @@
         }
     }
 
-    public class SavingsAccount : Account
+    public class SavingsAccount : AccountBase
     {
         public decimal InterestRate { get; set; }
 
         public override void Withdraw(decimal amount)
         {
             //Impose a withdrawal fee
-            WithdrawalAmount = amount + 5.0m;
-            base.Withdraw(WithdrawalAmount);
+            amount += 5.0m;
+
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+            }
+            else
+            {
+                throw new InvalidOperationException("Insufficient balance.");
+            }
         }
     }
 }
+
+
 
 
 //In this case, the SavingsAccount class violates the LSP because it changes the behavior of the Withdraw method it adds a withdrawal fee to the amount being withdrawn, which is not behavior that is expected from an Account.
